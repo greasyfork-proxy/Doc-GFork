723491287a2083a2f0bbc67f4e7edf978f076c84
Dahi <81472122+dahisea@users.noreply.github.com>
2025-04-06 20:41:48 +0800

    更新 index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,33 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: '一鍵安裝腳本',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Greasyfork 鏡像站提供流暢的使用者腳本安裝體驗，
+        只需單擊即可讓您喜愛的腳本在瀏覽器中運行。
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: '專注腳本體驗',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        無干擾瀏覽數以千計的審查過的使用者腳本，
+        我們保留了原始 Greasyfork 的所有功能，
+        同時提供更穩定的訪問體驗。
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: '完整腳本庫存',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        鏡像站同步 Greasyfork 所有審查過的腳本資源，
+        確保您能獲取最新、最全的腳本庫。
       </>
     ),
   },
